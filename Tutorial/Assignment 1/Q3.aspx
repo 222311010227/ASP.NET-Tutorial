882903327ce29d995caefd5ebc5fca933e446a71
Kuldip Zarmariya <kzarmariya711@rku.ac.in>
2024-12-27 23:08:50 +0530

    Remove style

--- Tutorial/Assignment 1/Q3.aspx
+++ Tutorial/Assignment 1/Q3.aspx
@@ -1,15 +1,47 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Q3.aspx.cs" Inherits="Tutorial.Assignment_1.Q3" %>
 
 <!DOCTYPE html>
-
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title></title>
+    <title>Algebraic Operations</title>
+
 </head>
 <body>
     <form id="form1" runat="server">
-        <div>
-        </div>
+        <h2>Algebraic Operations</h2>
+        
+        <!-- Input for First Number -->
+        <label for="txtNumber1">Enter First Value:&nbsp;&nbsp;&nbsp; </label>
+&nbsp;<asp:TextBox ID="txtNumber1" runat="server" Width="188px"></asp:TextBox>
+        
+        <!-- Input for Second Number -->
+        <label for="txtNumber2">
+        <br />
+        <br />
+        Enter Second Value:</label>
+        <asp:TextBox ID="txtNumber2" runat="server" Width="186px"></asp:TextBox>
+        
+        <!-- Dropdown for Operations -->
+        <label for="ddlOperation">
+        <br />
+        <br />
+        Select Operation:</label>
+        <asp:DropDownList ID="ddlOperation" runat="server">
+            <asp:ListItem Text="Addition" Value="Add" />
+            <asp:ListItem Text="Subtraction" Value="Subtract" />
+            <asp:ListItem Text="Multiplication" Value="Multiply" />
+            <asp:ListItem Text="Division" Value="Divide" />
+        </asp:DropDownList>
+        <br />
+        <br />
+        
+        <!-- Button to Perform Operation -->
+        <asp:Button ID="btnCalculate" runat="server" Text="Calculate" OnClick="btnCalculate_Click" />
+        
+        <!-- Label to Display Result -->
+        <br />
+        <br />
+        <asp:Label ID="lblResult" runat="server" CssClass="result-label"></asp:Label>
     </form>
 </body>
 </html>
--- Tutorial/Assignment 1/Q3.aspx.cs
+++ Tutorial/Assignment 1/Q3.aspx.cs
@@ -1,17 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace Tutorial.Assignment_1
 {
-    public partial class Q3 : System.Web.UI.Page
+    public partial class Q3 : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            // Optional: Add code here to run on page load if needed
+        }
 
+        protected void btnCalculate_Click(object sender, EventArgs e)
+        {
+            // Fetch input values
+            double number1, number2, result = 0;
+            string operation = ddlOperation.SelectedValue;
+
+            // Validate and parse inputs
+            if (double.TryParse(txtNumber1.Text, out number1) && double.TryParse(txtNumber2.Text, out number2))
+            {
+                // Perform selected operation
+                switch (operation)
+                {
+                    case "Add":
+                        result = number1 + number2;
+                        break;
+                    case "Subtract":
+                        result = number1 - number2;
+                        break;
+                    case "Multiply":
+                        result = number1 * number2;
+                        break;
+                    case "Divide":
+                        if (number2 != 0)
+                            result = number1 / number2;
+                        else
+                        {
+                            lblResult.Text = "Error: Division by zero is not allowed.";
+                            return;
+                        }
+                        break;
+                }
+
+                // Display the result
+                lblResult.Text = $"Result: {result}";
+            }
+            else
+            {
+                // Display error message if input is invalid
+                lblResult.Text = "Error: Please enter valid numbers.";
+            }
         }
     }
 }
