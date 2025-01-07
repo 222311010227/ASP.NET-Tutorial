1646d87abb7d0997731fb6f9e80f621e206cf207
Kuldip Zarmariya <kzarmariya711@rku.ac.in>
2025-01-07 11:23:35 +0530

    TT2 -Q2

---
+++ Tutorial/Assignment 2/Q2.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Q2.aspx.cs" Inherits="Tutorial.Assignment_2.Q2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Calendar Example</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Calendar ID="Calendar1" runat="server" OnSelectionChanged="Calendar1_SelectionChanged"></asp:Calendar>
+            <asp:Label ID="Label1" runat="server" Text="Selected Date: "></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Tutorial/Assignment 2/Q2.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Tutorial.Assignment_2
+{
+    public partial class Q2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Label1.Text = "Selected Date: " + Calendar1.SelectedDate.ToShortDateString();
+            }
+        }
+
+        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+        {
+            // Update the label with the selected date
+            Label1.Text = "Selected Date: " + Calendar1.SelectedDate.ToShortDateString();
+        }
+    }
+}
