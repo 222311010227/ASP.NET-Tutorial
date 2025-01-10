e11aa4fbb5d978ed7acb0fe0fd7a11bd28da9f79
Kuldip Zarmariya <kzarmariya711@rku.ac.in>
2025-01-10 09:11:12 +0530

    Q1

--- Tutorial/Assignment 2/Q1.aspx.cs
+++ Tutorial/Assignment 2/Q1.aspx.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 using System.Web.UI;
 
 namespace Tutorial.Assignment_2
@@ -8,37 +7,6 @@ namespace Tutorial.Assignment_2
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-        }
-
-        protected void UploadButton_Click(object sender, EventArgs e)
-        {
-            if (FileUpload1.HasFile)
-            {
-                try
-                {
-                    // Get the file name and set the upload path
-                    string filename = Path.GetFileName(FileUpload1.FileName);
-                    string uploadPath = Server.MapPath("~/Uploads/");
-
-                    // Ensure the directory exists
-                    if (!Directory.Exists(uploadPath))
-                    {
-                        Directory.CreateDirectory(uploadPath);
-                    }
-
-                    // Save the file to the server
-                    FileUpload1.SaveAs(Path.Combine(uploadPath, filename));
-                    UploadStatusLabel.Text = "File uploaded successfully!";
-                }
-                catch (Exception ex)
-                {
-                    UploadStatusLabel.Text = "File upload failed: " + ex.Message;
-                }
-            }
-            else
-            {
-                UploadStatusLabel.Text = "No file selected.";
-            }
         }
     }
 }
--- Tutorial/Assignment 2/Q1.aspx.designer.cs
+++ Tutorial/Assignment 2/Q1.aspx.designer.cs
@@ -31,32 +31,5 @@ namespace Tutorial.Assignment_2
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.AdRotator AdRotator1;
-
-        /// <summary>
-        /// FileUpload1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.FileUpload FileUpload1;
-
-        /// <summary>
-        /// UploadButton control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button UploadButton;
-
-        /// <summary>
-        /// UploadStatusLabel control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label UploadStatusLabel;
     }
 }
